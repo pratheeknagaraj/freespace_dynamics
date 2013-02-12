878232f16d46eb66f82da1ebd7f27a0c3e76322f
pratheeknagaraj <pratheek.nagaraj@gmail.com>
2013-02-12 00:28:05 -0800

    Create rk4_matrix.m

---
+++ rk4_matrix.m
@@ -0,0 +1,32 @@
+function R=rk4_matrix(f,a,b,ya,M)
+%Classical Runge-Kutta 4 Implementation
+%
+%Input   f - the function
+%        a - left endpoint
+%        b - right endpoint
+%        ya - initial condition y(a)
+%        M - number of steps
+%Ouput   R = [X' Y'] - X is vector of absicissas
+%                    - Y is vector of ordinates
+
+h = (b-a)/M; % Step Size
+X = zeros(1,M+1); % Absicissas Vector
+Y = zeros(3,M+1); % Ordinate Vector
+X = a:h:b; % Populate Absicissas Vector with intermediate points
+Y(:,1) = ya; % Set initial condition
+for j = 1:M % Define loop to iterate over steps
+    k1 = h * f(X(j),Y(:,j)); % Stage 1
+    % -- Insert Subprocess
+    k2 = h * f(X(j)+h/2,Y(:,j)+k1/2); % Stage 2
+    % -- Insert Subprocess --
+    k3 = h * f(X(j)+h/2,Y(:,j)+k2/2); % Stage 3
+    % -- Insert Subprocess --
+    k4 = h * f(X(j)+h,Y(:,j)+k3); % Stage 4
+    % -- Insert Subprocess --
+    Y(:,j+1) = Y(:,j)+(k1+2*k2+2*k3+k4)/6;
+end
+X
+Y
+
+R = [X' Y'];
+
